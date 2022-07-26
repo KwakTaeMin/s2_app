deaea112c0443579cf15db0694afb7d3ecd92467
tmkwak <taminging@kakao.com>
2022-07-26 15:18:05 +0900

    chat_repository.dart develop

--- lib/chat/repository/chat_repository.dart
+++ lib/chat/repository/chat_repository.dart
@@ -1,14 +1,18 @@
 
 import 'package:s2_app/chat/domain/favorite_word.dart';
+import 'package:s2_app/chat/domain/user_message_count.dart';
 import 'package:s2_app/chat/provider/chat_api_provider.dart';
 
 import '../domain/chat.dart';
+import '../domain/message_count.dart';
 
 class ChatRepository {
   final chatApiProvider = ChatApiProvider();
 
   Future<List<Chat>> get chats => chatApiProvider.getChats();
-
+  Future<List<MessageCount>> get messageCountGroupDate => chatApiProvider.getMessageCountGroupDate();
+  Future<List<UserMessageCount>> get userMessageCountGroupUserNameAndDate => chatApiProvider.getMessageCountGroupUserNameAndDate();
+  Future<int> get wordCount => chatApiProvider.getWordCount();
 
   Future<List<FavoriteWord>> getFavoriteWords(String? userName) {
     if(userName != null) {
@@ -23,6 +27,8 @@ class ChatRepository {
     }
     return chatApiProvider.getMessageCount();
   }
+
+
 
 
 
